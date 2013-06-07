64c5d5b50b06fc2a790cfaa990a421b282952a4a
Sergey <127932315+sergeythedeveloper@users.noreply.github.com>
2013-06-07 01:38:40 -0400

    Add feedback form

--- site.js
+++ site.js
@@ -14,4 +14,7 @@ var internshipLink = document.createElement('a');
 internshipLink.innerHTML = '<a href="https://intern.deteact.ru/">Apply here!</a>';
 document.body.appendChild(internshipLink);
 
-
+/* Create a feedback form at the bottom */
+var feedbackForm = document.createElement('div');
+feedbackForm.innerHTML = '<form>Name: <input type="text" name="name"><br>Email: <input type="text" name="email"><br>Message:<br><textarea rows="5" cols="40"></textarea><br><input type="submit" value="Submit"></form>';
+document.body.appendChild(feedbackForm);
