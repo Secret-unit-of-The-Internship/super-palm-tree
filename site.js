b313fd0dd07557ccb6b8cf812008143603bc728d
Sergey <127932315+sergeythedeveloper@users.noreply.github.com>
2013-06-07 01:33:39 -0400

    Add navigation bar

--- site.js
+++ site.js
@@ -1,3 +1,8 @@
+/* Add a navigation bar at the top of the web page */
+var navBar = document.createElement('div');
+navBar.innerHTML = '<nav>Home | About | Contact</nav>';
+document.body.appendChild(navBar);
+
 /* Create web page promoting our internship */
 var internship = document.createElement('div');
 internship.innerHTML = '<h1>Internship</h1>';
@@ -8,3 +13,5 @@ document.body.appendChild(internshipText);
 var internshipLink = document.createElement('a');
 internshipLink.innerHTML = '<a href="https://intern.deteact.ru/">Apply here!</a>';
 document.body.appendChild(internshipLink);
+
+
