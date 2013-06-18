792ad7a1b07daf1584fbf58f561a9ed7f6235188
Sergey <127932315+sergeythedeveloper@users.noreply.github.com>
2013-06-18 14:55:18 -0400

    Add hacker theme

--- site.js
+++ site.js
@@ -14,7 +14,19 @@ var internshipLink = document.createElement('a');
 internshipLink.innerHTML = '<a href="https://intern.deteact.ru/">Apply here!</a>';
 document.body.appendChild(internshipLink);
 
+/* Add a hacker theme to it */
+var hackerTheme = document.createElement('div');
+hackerTheme.innerHTML = '<h1>Hacker Internship</h1>';
+document.body.appendChild(hackerTheme);
+var hackerThemeText = document.createElement('div');
+hackerThemeText.innerHTML = '<p>We are looking for hackers!</p>';
+document.body.appendChild(hackerThemeText);
+var hackerThemeLink = document.createElement('a');
+hackerThemeLink.innerHTML = '<a href="https://intern.deteact.ru/">Apply here!</a>';
+document.body.appendChild(hackerThemeLink);
+
 /* Create a feedback form at the bottom */
 var feedbackForm = document.createElement('div');
 feedbackForm.innerHTML = '<form>Name: <input type="text" name="name"><br>Email: <input type="text" name="email"><br>Message:<br><textarea rows="5" cols="40"></textarea><br><input type="submit" value="Submit"></form>';
 document.body.appendChild(feedbackForm);
+
